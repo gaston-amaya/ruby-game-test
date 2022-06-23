b1ec8d3e822ef16e4ed99f2e2d037e93f1e3f1e2
gaston-amaya <amayagaston.sk@gmail.com>
2022-06-22 22:02:54 -0300

    testing hero animations, walk and cheer

--- window.rb
+++ window.rb
@@ -1,25 +1,43 @@
 require 'ruby2d'
 
+set title: "Story of ruby"
+
+background.new(
+  
+) 
+
 hero = Sprite.new(
-'Hero.png',
-width: 400,
-  height: 500,
-  clip_width: 100,
+'hero.png',
+width: 100,
+  height: 90,
+  clip_width: 78,
+  y: 390,
+  
   time: 250,
   animations: {
-    walk: 21..30,
-    jump: 1..2
-}
+    walk: 1..2,
+    cheer: 6..7,
+   }
+   
 )
-on :key_down do |event|
+on :key_held do |event|
     case event.key
-      when 'left'
-        hero.play animation: :walk, loop: true, flip: :horizontal
-      when 'right'
-        hero.play animation: :walk, loop: true
-      when 'up'
-      end
+      when 'a'
+        hero.play animation: :walk, flip: :horizontal
+      when 'd'
+        hero.play animation: :walk
     end
+  end
+
+  on :key_down do |event|
+    case event.key
+      when 'c'
+        hero.play animation: :cheer
+    end
+  end
+
+
+
 
 
 show
